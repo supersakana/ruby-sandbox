e07137baa5f337882d1ff7f6900dd68dd1159c1a
supersakana <zacwilliamson@icloud.com>
2022-02-11 12:29:30 -0600

    took some notes on new array methods

--- sandbox.rb
+++ sandbox.rb
@@ -22,3 +22,27 @@ puts age + 30
 end
 
 # Get the user's first and last name 
+puts 'What is your first name?'
+first_name = gets.chomp
+
+puts 'What is your last name?'
+last_name = gets.chomp
+
+puts 'Your full name is: ' + first_name + ' '  + last_name
+
+# Number between 0-100
+
+puts 'Please enter a number 0 - 100'
+number = gets.chomp.to_i
+
+if number <= 50
+    puts 'Your number is less than 50'
+elseif number <= 100 
+    puts 'Your number value is between 51 and 100'
+elseif number > 100 
+    puts 'Your number is greater than 100'
+else 
+    puts 'You cannot put a negative number'
+end
+
+    
