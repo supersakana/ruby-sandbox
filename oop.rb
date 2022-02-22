d101f04bccd5e207ab73ba4dd0ee482a55142663
supersakana <zacwilliamson@icloud.com>
2022-02-22 09:10:59 -0600

    added default params to human initilaizer

--- oop.rb
+++ oop.rb
@@ -5,23 +5,23 @@ module Life
     end
 end
 
-
 class Humans
     include Life
 
     attr_accessor :name, :age
 
-    def initialize(name, age)
+    def initialize(name = "Stranger", age = "immortal")
         @name = name
         @age = age
+        puts "Hello #{name}, you are #{age}. Welcome to the club "
     end
 end
 
 zac = Humans.new("Zac", 22)
 
-puts zac.age
-puts zac.name
-zac.name  = "Zachary"
-zac.speak("What's up big pimps")
-puts zac.name
+default = Humans.new
+
+# puts zac.age
+# puts zac.name
+# zac.speak("What's up big pimps")
 
