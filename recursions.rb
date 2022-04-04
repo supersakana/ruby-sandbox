eef8b793ebcc41a3cfe3f28c9639679de1655d08
supersakana <zacwilliamson@icloud.com>
2022-04-04 16:19:50 -0600

    added BBoR rock examples of recursion

--- recursions.rb
+++ recursions.rb
@@ -89,3 +89,33 @@ def print_children_recursive(tree)
 end
 
 print_children_recursive(TREE)
+
+# <--- Rock Examples --->
+# Loop
+# make some random rocks
+rocks = 30.times.map { rand(1..200) }
+puts rocks.join(', ')
+max_rock = 0
+
+rocks.each do |rock|
+  max_rock = rock if max_rock < rock
+end
+
+puts "Heaviest rock is: #{max_rock}"
+
+## or with inject
+puts "Heaviest rock is: #{rocks.inject { |max_rock, rock| max_rock > rock ? max_rock : rock }}"
+
+#  Recursion
+
+def rock_judger(rocks_arr)
+  if rocks_arr.length <= 2  # the base case
+    a = rocks_arr[0]
+    b = rocks_arr[-1]
+  else
+    a = rock_judger(rocks_arr.slice!(0, rocks_arr.length / 2))
+    b = rock_judger(rocks_arr)
+  end
+
+  a > b ? a : b
+end
