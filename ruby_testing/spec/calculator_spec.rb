d06c97adf11f69924270e6300b3cb1ebd8736dc2
supersakana <zacwilliamson@icloud.com>
2022-05-03 13:22:51 -0600

    finished tests for our calculator class

--- ruby_testing/spec/calculator_spec.rb
+++ ruby_testing/spec/calculator_spec.rb
@@ -26,6 +26,30 @@ describe Calculator do
       expect(calculator.multiply(2, 3, 4)).to eql(24)
     end
   end
+
+  describe '#subtract' do
+    it 'returns the difference between two numbers' do
+      calculator = Calculator.new
+      expect(calculator.subtract(7, 4)).to eql(3)
+    end
+
+    it 'returns the difference between more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.subtract(10, 2, 4)).to eql(4)
+    end
+  end
+
+  describe 'divide' do
+    it 'returns the quotient between two numbers' do
+      calculator = Calculator.new
+      expect(calculator.divide(6, 3)).to eql(2)
+    end
+
+    it 'returns the quotient of more than 2 numbers' do
+      calculator = Calculator.new
+      expect(calculator.divide(12, 3, 2)).to eql(2)
+    end
+  end
 end
 
 # describe is an RSpec keyword that defines an "Example Group", or a collection of tests. Takes a class or string and is passed a block (do/end).
